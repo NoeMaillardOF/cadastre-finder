cb362afecc3ec6dd8019d9e5e6ab4b729a509fda
noe.maillard <noe.maillard@ouest-france.fr>
2025-10-21 16:45:11 +0200

    setup gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/cadastre-finder/' : '/',
   plugins: [svelte()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
